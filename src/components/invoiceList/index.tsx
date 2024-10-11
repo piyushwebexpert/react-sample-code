4aad0c2e6d2d3e1157b79b0264d99d83d6a25998
Piyush <piyush.webexpert@gmail.com>
2024-10-11 22:03:30 +0530

    changes in index file

--- src/components/invoiceList/index.tsx
+++ src/components/invoiceList/index.tsx
@@ -40,8 +40,9 @@ function InvoiceList({
   };
 console.log("object")
 console.log("object")
-console.log("object")
 
+console.log("object")
+console.log("object")
 
   const handleClose = () => {
     setOpen(false);
